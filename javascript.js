2529887c7ae36cf6501d0eb2f52450383f3a0d32
Mike Costa <mcosta1@gmail.com>
2023-02-22 21:55:09 -0500

    Add createGameStatus function to javascript.js
    This function will display the following information during the game:
    	Round 1 of N
    	Player: 0
    	Computer: 0
    	Ties: 0
    
    These will be dynamically updated as the game progresses.
    
    Also renamed the getGameSetup function to createGameSetup.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function getGameSetup() {
+function createGameSetup() {
   const setupContainer = document.createElement('div');
   setupContainer.classList.add('game-setup');
 
@@ -40,5 +40,57 @@ function getGameSetup() {
   return setupContainer;
 }
 
+function createGameStatus(numberOfRounds) {
+  const gameStatus = document.createElement('div');
+  gameStatus.classList.add('game-status');
+  
+  const roundInfo = document.createElement('p');
+
+  const currentRound = document.createElement('span');
+  currentRound.setAttribute('id', 'current-round');
+  currentRound.textContent = '1';
+
+  const lastRound = document.createElement('span');
+  lastRound.setAttribute('id', 'last-round');
+  
+  if(!isNaN(numberOfRounds)) {
+    lastRound.textContent = numberOfRounds.toString();
+  } else {
+    lastRound.textContent = '1';
+  }
+  
+  roundInfo.appendChild(new Text('Round '));
+  roundInfo.appendChild(currentRound);
+  roundInfo.appendChild(new Text(' of '));
+  roundInfo.appendChild(lastRound);
+  gameStatus.appendChild(roundInfo);
+  
+  const playerScore = document.createElement('p');
+  const playerPoints = document.createElement('span');
+  playerPoints.setAttribute('id', 'player-points');
+  playerPoints.textContent = '0';
+  playerScore.appendChild(new Text('Player: '));
+  playerScore.appendChild(playerPoints);
+  gameStatus.appendChild(playerScore);
+  
+  const computerScore = document.createElement('p');
+  const computerPoints = document.createElement('span');
+  computerPoints.setAttribute('id', 'computer-points');
+  computerPoints.textContent = '0';
+  computerScore.appendChild(new Text('Computer: '));
+  computerScore.appendChild(computerPoints);
+  gameStatus.appendChild(computerScore);
+  
+  const tieScore = document.createElement('p');
+  const tiePoints = document.createElement('span');
+  tiePoints.setAttribute('id', 'tie-points');
+  tiePoints.textContent = '0';
+  tieScore.appendChild(new Text('Ties: '));
+  tieScore.appendChild(tiePoints);
+  gameStatus.appendChild(tieScore);
+  
+  return gameStatus;
+}
+
 const gameContainer = document.querySelector('.game-container');
-gameContainer.appendChild(getGameSetup());
+gameContainer.appendChild(createGameStatus(10));
